aaf31a73d7dfe4b6635540030250ef689efc1fb6
persianpros <persianpros@yahoo.com>
2021-04-14 14:59:48 +0430

    PEP8 double aggressive E251 and E252

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 import setup_translate
 
 pkg = 'Extensions.BlackoutBlind'
-setup (name = 'enigma2-plugin-extensions-blackoutblind',
-       version = '1.3.1',
-       description = 'hide white dotted (VBI) lines on top of the screen',
-       packages = [pkg],
-       package_dir = {pkg: 'plugin'},
-       package_data = {pkg: ['*.png', 'locale/*/LC_MESSAGES/*.mo']},
-       cmdclass = setup_translate.cmdclass, # for translation
+setup (name='enigma2-plugin-extensions-blackoutblind',
+       version='1.3.1',
+       description='hide white dotted (VBI) lines on top of the screen',
+       packages=[pkg],
+       package_dir={pkg: 'plugin'},
+       package_data={pkg: ['*.png', 'locale/*/LC_MESSAGES/*.mo']},
+       cmdclass=setup_translate.cmdclass, # for translation
       )
